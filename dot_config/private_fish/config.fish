55a50a2bbd05d4a0545965cdf3faffac383ac6db
lvlcn-t <75443136+lvlcn-t@users.noreply.github.com>
2025-12-04 19:12:23 +0100

    feat: add fish config
    Signed-off-by: lvlcn-t <75443136+lvlcn-t@users.noreply.github.com>

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,3 +3,20 @@ if status is-interactive
 end
 
 starship init fish | source
+source $HOME/.env.fish
+
+bind tab complete-and-search
+bind ctrl-j forward-char
+bind ctrl-h backward-kill-path-component
+
+set -g -x EDITOR nvim
+set -g -x GOBIN $HOME/.local/bin
+
+fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/.local/bin
+fish_add_path $HOME/.local/go/bin
+fish_add_path $HOME/.local/nvim/bin
+fish_add_path $HOME/.local/krew/bin
+
+alias fzfb="fzf_configure_bindings --help"
+alias gcsm="git commit --signoff -m"
